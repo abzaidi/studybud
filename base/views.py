da5679bb8d3b694ebd52ad7f9efb62b4e4f92917
Abubakar Zaidi <ma2003110@gmail.com>
2024-06-30 22:38:46 +0500

    authenticate user login and flash messages

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
@@ -12,6 +14,22 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password does not exist')
     context = {}
     return render(request, 'base/login_register.html', context)
 
