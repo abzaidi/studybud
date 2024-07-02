8e5643fa0f46710891ffe3ff8480fd4637721add
Abubakar Zaidi <ma2003110@gmail.com>
2024-07-02 22:39:36 +0500

    implement user registration

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .models import Room, Topic
@@ -16,6 +17,7 @@ from .forms import RoomForm
 
 def loginPage(request):
 
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -34,12 +36,26 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username OR Password does not exist')
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration!')
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
